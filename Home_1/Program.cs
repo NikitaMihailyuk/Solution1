9f5369f20acc98f95f67f2524b3bc395d2bf081d
Никита <isthisnikita@gmail.com>
2023-04-04 20:15:20 +0300

    Правки пространства имён

---
+++ Home_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            var user = Console.ReadLine();
+            Console.WriteLine($"Hello,{user}");
+            var stoper = Console.ReadLine();
+        }
+    }
+}
