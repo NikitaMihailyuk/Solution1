282c0171e304701e71b1a18b2d526503e2454d41
Никита <isthisnikita@gmail.com>
2023-04-17 03:28:22 +0300

    Home_6

---
+++ Home_6/ShowInformation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_6
+{
+    public class ShowInformation
+    {
+        public void ShowPatients(Patient[] patients)
+        {
+            foreach (Patient pat in patients)
+            {
+                pat.TreatmentInfo();
+            }
+        }
+    }
+}
