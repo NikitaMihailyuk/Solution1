b6dd985412f8392f59533bd21116885350d01c78
Никита <isthisnikita@gmail.com>
2023-05-23 01:36:07 +0300

    Home 10. Part 2

--- Home_10/Program.cs
+++ Home_10/Program.cs
@@ -26,41 +26,15 @@ public static void ShowPrice(int price)
 2. Реализовать паттерн наблюдатель в случае если цена на жилье опустилась ниже определенного значения, и сообщить всем кто на это событие подписался. 
 * реализовать как через событие так и через паттерн наблюдатель без событи
 https://refactoring.guru/ru/design-patterns/observer
+ */
 
-Linq
-Реализовать следующие методы:
-- Метод возвращает первое слово из последовательности слов, содержащее только одну букву.
-- метод, возвращающий последнее слово, содержащее в себе подстроку «ее». Реализовать, используя только 1 метод LINQ.
-- Реализовать метод для возврата последнего слова, соответствующего условию: длина слова не меньше min и не больше max. Если нет слов, соответствующих условию, метод возвращает null.
-- Напишите метод, который возвращает количество уникальных значений в массиве.
-- Напишите метод, который принимает список и извлекает значения с 5  элемента (включительно)  те значение которые содержат 3
-- Метод возвращает длину самого короткого слова
-- Напишите метод, который преобразует словарь в список и меняет местами каждый ключ и значение
-
-дан класс
-public class User
-    {
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-
-        public User(string firstName, string middleName, string lastName)
-        {
-            FirstName = firstName;
-            MiddleName = middleName;
-            LastName = lastName;
-        }
-    }
-- Напишите метод, который возвращает "<FirstName> <MiddleName> <LastName>", если отчество присутствует Или "<FirstName> <LastName>", если отчество отсутствует.
--Напишите метод, который возвращает предоставленный список пользователей, упорядоченный по LastName в порядке убывания.
-
-        */
         static void Main(string[] args)
         {
             PriceMonitor priceMonitor = new PriceMonitor();
             HomeCreator creator = new HomeCreator();
             Listner1 listner1 = new Listner1();
             Listner2 listner2 = new Listner2();
+            linqtasks linqtasks = new linqtasks();
 
             Home[] homes = creator.HomeCreates();
             Console.WriteLine("_______________________________________");
@@ -75,13 +49,26 @@ public class User
             Console.WriteLine("Show Avarage Price and all \n");
             priceMonitor.LogicPrice(homes, Typeinfo.Avarage);
 
-
             /// Price go down
             priceMonitor.notify += listner1.SellNotification;
             priceMonitor.notify += listner2.SellNotification;
-            priceMonitor.PriceGoDown(homes); 
-     
+            priceMonitor.PriceGoDown(homes);
+
+  
+            /// linq Tasks
+            linqtasks.Task1();
+            linqtasks.Task2();
+            linqtasks.Task3();
+            linqtasks.Task4();
+            linqtasks.Task5();
+            linqtasks.Task6();
+            linqtasks.Task7();
+            linqtasks.Task8();
+
             Console.ReadKey();
         }
+
+        
+
     }
 }
