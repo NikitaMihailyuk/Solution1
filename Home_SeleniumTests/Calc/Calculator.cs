a551dc50ca58842acbe1fb2d37d4763e252de0f2
Никита <isthisnikita@gmail.com>
2023-05-23 01:52:51 +0300

    Home_11

---
+++ Home_SeleniumTests/Calc/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_SeleniumTests.Calc
+{
+    public class Calculator
+    {
+        public double Add(double x, double y) => x + y;
+        public double Multiply(double x, double y) => x * y;
+    }
+}
